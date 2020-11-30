dc3c7f21271bb77f18acc80c5d5359ac3be1cfee
Hitesh <hitgo17@gmail.com>
2020-11-30 23:26:06 +0530

    feat: adds logic to allow only @daiitc.ac.in logins
    Signed-off-by: Hitesh <hitgo17@gmail.com>

--- screens/SignIn.js
+++ screens/SignIn.js
@@ -1,9 +1,9 @@
-import React from "react";
-import { View, Button } from "react-native";
-import * as Google from "expo-google-app-auth";
-import { AuthContext } from "../components/context";
-import { SocialIcon, Text } from "react-native-elements";
-import {Android_ID,IOS_ID} from "@env"
+import React from 'react';
+import { View, Button } from 'react-native';
+import * as Google from 'expo-google-app-auth';
+import { AuthContext } from '../components/context';
+import { SocialIcon, Text } from 'react-native-elements';
+import { Android_ID, IOS_ID } from '@env';
 
 export default function SignIn() {
   const { authcontext } = React.useContext(AuthContext);
@@ -11,28 +11,37 @@ export default function SignIn() {
 
   const signInWithGoogleAsync = async () => {
     try {
-      console.log("Try..");
+      console.log('Try..');
       const result = await Google.logInAsync({
-        //  behavior: "web",
-        androidClientId:Android_ID,
-        iosClientId:IOS_ID,
-        scopes: ["profile", "email"],
-        hostedDomain: "daiict.ac.in",
+        behavior: 'web',
+        androidClientId: Android_ID,
+        iosClientId: IOS_ID,
+        scopes: ['profile', 'email'],
+        hostedDomain: 'daiict.ac.in',
+        androidStandaloneAppClientId: Android_ID,
       });
 
-      if (result.type === "success") {
-        signIn(result);
+      if (result.type === 'success') {
         console.log(result);
+        if (result.user.email.split('@')[1] !== 'daiict.ac.in') {
+          alert('Please use @daiict.ac.in email only');
+          return { cancelled: true };
+        } else {
+          signIn(result);
+        }
+
         return result.idToken;
       } else {
+        console.log('error');
         return { cancelled: true };
       }
     } catch (e) {
+      console.log(e);
       return { error: true };
     }
   };
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text h1>Connect</Text>
       <Text style={{ marginTop: 30 }}>It's exclusive!</Text>
       <SocialIcon
