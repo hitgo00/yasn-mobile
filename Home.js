90fa372c12b862a23539e2e099579a9017f8cc8b
Hitesh <hitgo17@gmail.com>
2020-12-02 20:16:58 +0530

    feat: updates bottomNavigation UI
    Signed-off-by: Hitesh <hitgo17@gmail.com>

--- Home.js
+++ Home.js
@@ -1,64 +1,58 @@
-import React, { useEffect,useState } from "react";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import Feed from "./screens/Feed";
-import Profile from "./screens/Profile";
-import Chat from "./screens/Chat";
-import AddPost from "./screens/AddPost";
-import { Container, Text, Content, Header, Body } from "native-base";
-import { NavigationContainer } from "@react-navigation/native";
-import Icon from "react-native-vector-icons/Ionicons";
-import { AuthContext } from "./components/context";
-import { AsyncStorage,ActivityIndicator } from "react-native";
-import {  Provider, Modal, Portal} from 'react-native-paper';
-import UserDetailForm from "./components/UserDetailForm";
+import React, { useEffect, useState } from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Feed from './screens/Feed';
+import Profile from './screens/Profile';
+import Chat from './screens/Chat';
+import AddPost from './screens/AddPost';
+import { Container, Text, Content, Header, Body } from 'native-base';
+import { NavigationContainer } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import { AuthContext } from './components/context';
+import { AsyncStorage, ActivityIndicator } from 'react-native';
+import { Provider, Modal, Portal } from 'react-native-paper';
+import UserDetailForm from './components/UserDetailForm';
 import axios from 'axios';
 import queryString from 'query-string';
 
 const Tab = createBottomTabNavigator();
 
-
-
 export default function Home() {
-
   const { authcontext, loginState } = React.useContext(AuthContext);
-  const { signOut,SetUserDetails_server } = authcontext;
+  const { signOut, SetUserDetails_server } = authcontext;
 
-  const [isLoading, setLoading]=useState(true);
-  const [profile,setProfile]=useState(false);
-  const [visible,setVisible]=useState(false);
+  const [isLoading, setLoading] = useState(true);
+  const [profile, setProfile] = useState(false);
+  const [visible, setVisible] = useState(false);
 
-  const API_URL="https://connectda.herokuapp.com"
+  const API_URL = 'https://connectda.herokuapp.com';
 
-  useEffect(()=>{
-    console.log("Get request Again..");
+  useEffect(() => {
+    console.log('Get request Again..');
     axios
       .get(
         `${API_URL}/checkprofile?` +
           queryString.stringify(
-            { email:loginState.user.email, googleToken:loginState.userToken },
+            { email: loginState.user.email, googleToken: loginState.userToken },
             { withCredentials: true }
           )
       )
       .then((res) => {
-       // cookies.set('userDetails', res.data[0]);
-        console.log("response : ",res.data);
+        // cookies.set('userDetails', res.data[0]);
+        console.log('response : ', res.data);
         if (res.data === 'invalid token') {
-          console.log("Yess Called...");
+          console.log('Yess Called...');
           signOut();
-        }
-        else if (res.data===false) {
-          
+        } else if (res.data === false) {
           setLoading(false);
           setVisible(true);
-        }
-        else{
+        } else {
           SetUserDetails_server(res.data[0]);
           setLoading(false);
           setVisible(false);
           setProfile(true);
         }
       });
-  },[visible])
+  }, [visible]);
 
   return (
     <NavigationContainer>
@@ -68,30 +62,49 @@ export default function Home() {
         </Body>
       </Header>
 
-    {isLoading?<ActivityIndicator style={{flex:1,justifyContent:'center',alignItems:'center'}} size='large'/>:
-
-      profile?
+      {isLoading ? (
+        <ActivityIndicator
+          style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
+          size="large"
+        />
+      ) : profile ? (
         <Tab.Navigator
           screenOptions={({ route }) => ({
             tabBarIcon: ({ focused, color, size }) => {
               let iconName;
 
-              if (route.name === "Feed") {
-                iconName = focused ? "ios-compass" : "ios-compass";
-              } else if (route.name === "Profile") {
-                iconName = focused ? "ios-contact" : "ios-contact";
-              } else if (route.name === "AddPost") {
-                iconName = focused ? "ios-add-circle" : "ios-add-circle";
-              } else if (route.name === "Chat") {
-                iconName = focused ? "ios-chatboxes" : "ios-chatboxes";
+              if (route.name === 'Feed') {
+                iconName = focused ? 'ios-compass' : 'ios-compass';
+              } else if (route.name === 'Profile') {
+                iconName = focused ? 'ios-contact' : 'ios-contact';
+              } else if (route.name === 'AddPost') {
+                iconName = focused ? 'ios-add-circle' : 'ios-add-circle';
+              } else if (route.name === 'Chat') {
+                iconName = focused ? 'ios-chatboxes' : 'ios-chatboxes';
               }
               // You can return any component that you like here!
-              return <Icon name={iconName} size={28} color={color} />;
+              return (
+                <Icon
+                  name={iconName}
+                  size={28}
+                  color={focused ? 'black' : color}
+                />
+              );
             },
           })}
           tabBarOptions={{
-            activeTintColor: "#208af5",
-            inactiveTintColor: "gray",
+            activeTintColor: '#208af5',
+            inactiveTintColor: '#D0D0D0',
+
+            style: {
+              marginHorizontal: 8,
+              marginBottom: 3,
+              borderRadius: 14,
+              position: 'absolute',
+            },
+            labelStyle: {
+              color: 'black',
+            },
           }}
         >
           <Tab.Screen name="Feed" component={Feed} />
@@ -99,19 +112,21 @@ export default function Home() {
           <Tab.Screen name="Chat" component={Chat} />
           <Tab.Screen name="Profile" component={Profile} />
         </Tab.Navigator>
-        :
+      ) : (
         <Provider>
           <Portal>
             <Modal visible={visible}>
               <UserDetailForm
                 email={loginState.user.email}
                 googleToken={loginState.userToken}
-                myFun={(val)=>{setVisible(val)}}
+                myFun={(val) => {
+                  setVisible(val);
+                }}
               />
             </Modal>
           </Portal>
         </Provider>
-      }
+      )}
     </NavigationContainer>
   );
 }
--- screens/Feed.js
+++ screens/Feed.js
@@ -1,44 +1,44 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import queryString from "query-string";
-import { StyleSheet, View, SafeAreaView, Alert } from "react-native";
-import { Container, Text, Content, Header, Body } from "native-base";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import queryString from 'query-string';
+import { StyleSheet, View, SafeAreaView, Alert } from 'react-native';
+import { Container, Text, Content, Header, Body } from 'native-base';
 //import PostCard from "../components/PostCard";
-import PostCard from "../components/PostCard/index";
-import { AuthContext } from "../components/context";
+import PostCard from '../components/PostCard/index';
+import { AuthContext } from '../components/context';
 //import { API_URL } from "@env";
 import { useFocusEffect } from '@react-navigation/native';
 
-
 export default (Feed = () => {
-  const [browseTag, SetBrowseTag] = useState("");
+  const [browseTag, SetBrowseTag] = useState('');
   const [posts, setPosts] = useState();
   const { loginState } = React.useContext(AuthContext);
 
-   const email=loginState.user.email;
-  const googleToken=loginState.userToken;
-  console.log("From Feed : ",loginState);
-  const API_URL="https://connectda.herokuapp.com";
-  console.log("Feed: ",API_URL);
+  const email = loginState.user.email;
+  const googleToken = loginState.userToken;
+  console.log('From Feed : ', loginState);
+  const API_URL = 'https://connectda.herokuapp.com';
+  console.log('Feed: ', API_URL);
   useFocusEffect(
     React.useCallback(() => {
       axios
-      .get(
-        `${API_URL}/home?` +
-          queryString.stringify({ tag: browseTag ,email,googleToken}, { withCredentials: true })
-      )
-      .then((res) => {
-        if(res.data!=="invalid token"){
-          console.log("========Again=======");
-          setPosts(res.data);
-        }
-        else{
-          Alert.alert("Your Session is Expired","Please Login Again.");
-        }
-        
-      })
+        .get(
+          `${API_URL}/home?` +
+            queryString.stringify(
+              { tag: browseTag, email, googleToken },
+              { withCredentials: true }
+            )
+        )
+        .then((res) => {
+          if (res.data !== 'invalid token') {
+            console.log('========Again=======');
+            setPosts(res.data);
+          } else {
+            Alert.alert('Your Session is Expired', 'Please Login Again.');
+          }
+        })
 
-      .catch((err) => console.log(err));
+        .catch((err) => console.log(err));
     }, [])
   );
 
@@ -46,31 +46,34 @@ export default (Feed = () => {
     axios
       .get(
         `${API_URL}/home?` +
-          queryString.stringify({ tag: browseTag ,email,googleToken}, { withCredentials: true })
+          queryString.stringify(
+            { tag: browseTag, email, googleToken },
+            { withCredentials: true }
+          )
       )
       .then((res) => {
-        if(res.data!=="invalid token"){
-          console.log("........................................Again..........................................................");
+        if (res.data !== 'invalid token') {
+          console.log(
+            '........................................Again..........................................................'
+          );
           setPosts(res.data);
+        } else {
+          Alert.alert('Your Session is Expired', 'Please Login Again.');
         }
-        else{
-          Alert.alert("Your Session is Expired","Please Login Again.");
-        }
-        
       })
 
       .catch((err) => console.log(err));
-  }, []); 
-//  console.log(posts);
+  }, []);
+  //  console.log(posts);
 
   {
     /* <SafeAreaView style={{ flex: 1 }}>{<GoogleSignIn />}</SafeAreaView>; */
   }
   return (
-    <Content>
-      { posts
+    <Content style={{ backgroundColor: '#000000' }}>
+      {posts
         ? posts.map((post) => <PostCard {...post} key={post._id} />)
-        : null} 
+        : null}
     </Content>
   );
 });
